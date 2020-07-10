712579a0395ad47e0551b7a14b01d54da7801411
szerholder <lisadesh1@gmail.com>
2020-07-10 04:54:41 +0200

    usuniecie slasha przeszkadzajacacego w CORS policy

--- src/main/java/com/ad44/springboot/er/MyRestController.java
+++ src/main/java/com/ad44/springboot/er/MyRestController.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 
-@CrossOrigin(origins = {"https://easyrentier-front.herokuapp.com/"})
+@CrossOrigin(origins = {"https://easyrentier-front.herokuapp.com"})
 @RestController("/")
 public class MyRestController {
 
